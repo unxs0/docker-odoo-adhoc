7e615aed854ba6f6c22fc4686a05b02846f20f9a
root <root@rancher.c.adhoc-dev.internal>
2017-04-17 15:56:31 +0000

    Fixed this mess by using docker.sock

--- rancher-gce/nginx-ubuntu/certbot.sh
+++ rancher-gce/nginx-ubuntu/certbot.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+CERTBOT_EMAIL=`/usr/sbin/dockprox --email`;
+CERTBOT_DOMAIN=`/usr/sbin/dockprox --domains | cut -f 1 -d ' '`;
+CERTBOT_DOMAINS=`/usr/sbin/dockprox --domains`;
+
+echo "start CERTBOT_EMAIL=$CERTBOT_EMAIL CERTBOT_DOMAIN=$CERTBOT_DOMAIN";
+
 if [ "$CERTBOT_EMAIL" != "" ] && [ "$CERTBOT_DOMAIN" != "" ];then
 	grep rancher.your.io /etc/nginx/conf.d/default.conf > /dev/null 2>&1;
 	if [ "$?" == "0" ];then
 		sed -i s/rancher.your.io/$CERTBOT_DOMAIN/g /etc/nginx/conf.d/default.conf;
 	fi
-	/root/certbot-auto --nginx --email $CERTBOT_EMAIL --agree-tos --non-interactive --domains $CERTBOT_DOMAIN > /dev/null 2>&1;
+	/root/certbot-auto --nginx --email $CERTBOT_EMAIL --agree-tos --non-interactive --domains $CERTBOT_DOMAINS > /tmp/certbot.sh.log 2>&1;
+else
+	echo "No CERTBOT_EMAIL CERTBOT_DOMAIN env vars" > /tmp/certbot.sh.log;
 fi
